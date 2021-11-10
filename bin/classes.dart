897dcf6e142e89050834f9ad3da8e8d173b2a005
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-11-10 15:44:29 +0200

    refactor user class to its own file

--- bin/classes.dart
+++ bin/classes.dart
@@ -1,3 +1,5 @@
+import 'user.dart';
+
 void main(List<String> arguments) {
   final user = User(id: 45, name: 'Blessed Sibanda');
   print(user);
@@ -5,24 +7,4 @@ void main(List<String> arguments) {
   final anonymousUser = User.anonymous();
   print(anonymousUser);
   print(anonymousUser.toJson());
-}
-
-class User {
-  User({int id = 0, String name = 'anonymous'})
-      : _id = id,
-        _name = name;
-
-  User.anonymous() : this();
-
-  int _id;
-  String _name;
-
-  String toJson() {
-    return '{"id": "$_id", "name": "$_name"}';
-  }
-
-  @override
-  String toString() {
-    return 'User(id: $_id, name: $_name)';
-  }
 }
---
+++ bin/user.dart
@@ -0,0 +1,19 @@
+class User {
+  User({int id = 0, String name = 'anonymous'})
+      : _id = id,
+        _name = name;
+
+  User.anonymous() : this();
+
+  int _id;
+  String _name;
+
+  String toJson() {
+    return '{"id": "$_id", "name": "$_name"}';
+  }
+
+  @override
+  String toString() {
+    return 'User(id: $_id, name: $_name)';
+  }
+}
