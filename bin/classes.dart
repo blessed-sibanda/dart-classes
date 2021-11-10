69ae1aa99ed7423d1683005b53c724fd9a549c17
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-11-10 15:22:22 +0200

    named constructors

--- bin/classes.dart
+++ bin/classes.dart
@@ -2,13 +2,20 @@ void main(List<String> arguments) {
   final user = User(45, 'Blessed Sibanda');
   print(user);
   print(user.toJson());
+  final anonymousUser = User.anonymous();
+  print(anonymousUser);
 }
 
 class User {
   User(this.id, this.name);
 
-  int id;
-  String name;
+  User.anonymous() {
+    id = 0;
+    name = 'anonymous';
+  }
+
+  int id = 0;
+  String name = '';
 
   String toJson() {
     return '{"id": "$id", "name": "$name"}';
